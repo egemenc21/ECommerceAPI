4d7b1d619e02698f1a6e2fe1e86f5a077ce101ab
Egemen <egemenc2101@gmail.com>
2024-08-21 16:04:34 +0300

    Added Pagination.cs file for implementing pagination logic to products

--- Presentation/ECommerceAPI.API/Controllers/ProductController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Application.RequestParameters;
 using ECommerceAPI.Application.ViewModels.Products;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,15 +25,29 @@ namespace ECommerceAPI.API.Controllers
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
-
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false));
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).Skip(pagination.Page * pagination.Size).Take(pagination.Size);
+            
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
         }
-        
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
@@ -42,11 +57,6 @@ namespace ECommerceAPI.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if (ModelState.IsValid)
-            {
-                
-            }
-            
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
@@ -55,8 +65,8 @@ namespace ECommerceAPI.API.Controllers
             });
 
             await _productWriteRepository.SaveAsync();
-            
-            return StatusCode((int) HttpStatusCode.Created);
+
+            return StatusCode((int)HttpStatusCode.Created);
         }
 
         [HttpPut]
@@ -67,8 +77,8 @@ namespace ECommerceAPI.API.Controllers
             product.Name = model.Name;
             product.Price = model.Price;
             await _productWriteRepository.SaveAsync();
-            
-            
+
+
             return Ok(_productWriteRepository);
         }
 
@@ -79,7 +89,5 @@ namespace ECommerceAPI.API.Controllers
             await _productWriteRepository.SaveAsync();
             return Ok();
         }
-         
-        
     }
 }
